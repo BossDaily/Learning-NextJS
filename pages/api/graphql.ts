4b812bf1476bc1bec2e5c4ae34518039bf811898
BossDaily <comparatorcraft2020@gmail.com>
2022-05-21 16:26:30 -0500

    linter

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,30 +1,29 @@
-import { ApolloServer } from 'apollo-server-micro'
-import { typeDefs } from '../../graphql/schema'
-import { resolvers } from '../../graphql/resolvers'
-import Cors from 'micro-cors'
+import { ApolloServer } from "apollo-server-micro";
+import { typeDefs } from "../../graphql/schema";
+import { resolvers } from "../../graphql/resolvers";
+import Cors from "micro-cors";
 
-const cords = Cors()
+const cords = Cors();
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-const startServer = apolloServer.start()
+const startServer = apolloServer.start();
 
 export default Cors(async function handler(req, res) {
-    if(req.method === 'OPTIONS'){
-        res.end()
-        return false
-    }
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
 
-    await startServer
-    
-    await apolloServer.createHandler({
-        path: '/api/graphql'
-    })(req,res)
+  await startServer;
 
-})
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+});
 
 export const config = {
-    api: {
-        bodyParser: false, 
-    },
-}
+  api: {
+    bodyParser: false,
+  },
+};
