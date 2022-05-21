ae041527750474c13263dd8e3877ba4a588bcea1
BossDaily <comparatorcraft2020@gmail.com>
2022-05-21 17:31:26 -0500

    context

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,2 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 import prisma from '../lib/prisma'
+
+export type Context = {
+    prisma: PrismaClient
+}
+
+const createContext = async ({ req, res }): Promise<Context> => {
+    return{
+        prisma, 
+    }
+}
