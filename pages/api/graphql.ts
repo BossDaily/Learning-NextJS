d53163b430c7fbd44290e056d608777114a96425
BossDaily <comparatorcraft2020@gmail.com>
2022-05-21 17:43:01 -0500

    changed import

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
-import { createContext } from "../../graphql/context";
+import  createContext  from "../../graphql/context";
 import Cors from "micro-cors";
 
 const cors = Cors();
