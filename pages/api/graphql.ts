c39dd5555af2762aa02ab5271ca8a76584b80674
BossDaily <comparatorcraft2020@gmail.com>
2022-05-21 16:09:55 -0500

    cors and config

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,3 +3,28 @@ import { typeDefs } from '../../graphql/schema'
 import { resolvers } from '../../graphql/resolvers'
 import Cors from 'micro-cors'
 
+const cords = Cors()
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers })
+
+const startServer = apolloServer.start()
+
+export default Cors(async function (req, res) {
+    if(req.method === 'OPTIONS'){
+        res.end()
+        return false
+    }
+
+    await startServer
+    
+    await apolloServer.createHandler({
+        path: '/api/graphql'
+    })(req,res)
+
+})
+
+export const config = {
+    api: {
+        bodyParser: false, 
+    },
+}
