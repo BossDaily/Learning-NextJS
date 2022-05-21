5ea6bbaffcf7cb4a34f5a0fe57334a3f22023d27
BossDaily <comparatorcraft2020@gmail.com>
2022-05-21 16:27:20 -0500

    fixed typos

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,13 +3,13 @@ import { typeDefs } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
 import Cors from "micro-cors";
 
-const cords = Cors();
+const cors = Cors();
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 const startServer = apolloServer.start();
 
-export default Cors(async function handler(req, res) {
+export default cors(async function handler(req, res) {
   if (req.method === "OPTIONS") {
     res.end();
     return false;
