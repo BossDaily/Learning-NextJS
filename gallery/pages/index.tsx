71c33a0893d1295355fd3084d26c9d6d855c45c3
BossDaily <gboys2008@gmail.com>
2022-05-27 12:57:29 -0500

    basic component

---
+++ gallery/pages/index.tsx
@@ -0,0 +1,8 @@
+import Images from "./_image";
+import Image from "next/image";
+import Gallery from "./_gallery";
+import { NextPage } from "next";
+
+const Home:NextPage = () => {
+  <Gallery />
+}
