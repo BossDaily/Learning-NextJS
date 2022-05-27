ba30b12100994b9d7893097211a4b094c917286f
BossDaily <gboys2008@gmail.com>
2022-05-27 12:41:16 -0500

    added image component

--- gallery/pages/index.tsx
+++ gallery/pages/index.tsx
@@ -1,15 +1,18 @@
-import type { NextPage } from 'next'
+import type { NextComponentType, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Gallery: NextPage = () => {
+const Gallery = () => {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        
+
       </div>
     </div>
   )
 }
 
+const Image = () => {
+
+}
 export default Gallery
