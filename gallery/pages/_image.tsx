c9725bc1359607c3b7f8deaadaf65d27b9aad32b
BossDaily <gboys2008@gmail.com>
2022-05-27 17:46:12 -0500

    changed to image component

--- gallery/pages/_image.tsx
+++ gallery/pages/_image.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image"
+
 const imageCard = () => {
     return (
       <a href='#' className='group'>
         <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
-          <img 
+          <Image 
             alt="" 
             src="https://avatars.githubusercontent.com/u/75937850?v=4"
+            layout="fill"
+            objectFit="cover"
             className="group-hover:opacity-75"
           />
           <h3 className="mt-4 text-sm text-gray-700">BossDaily</h3>
