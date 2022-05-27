af51c040eadf2daab974075ed847ccfb17d8249c
BossDaily <gboys2008@gmail.com>
2022-05-27 12:46:32 -0500

    css for title

--- gallery/pages/index.tsx
+++ gallery/pages/index.tsx
@@ -12,12 +12,13 @@ const Gallery = () => {
 const Image = () => {
   return (
     <a href='#' className='group'>
-      <div className='aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200'>
+      <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
         <img 
           alt="" 
           src="https://avatars.githubusercontent.com/u/75937850?v=4"
+          className="group-hover:opacity-75"
         />
-        
+        <h3 className="mt-4 text-sm text-gray-700">BossDaily</h3>
       </div>
     </a>
   )
