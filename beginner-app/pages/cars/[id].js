adec860570016e85ed0406f83be6b11c0136f165
BossDaily <gboys2008@gmail.com>
2022-05-28 16:15:46 -0500

    static props

--- beginner-app/pages/cars/[id].js
+++ beginner-app/pages/cars/[id].js
@@ -1,14 +1,27 @@
 import {useRouter} from 'next/router';
 
-const Car = () => {
+const Car = ({ car }) => {
     const router = useRouter()
     const { id } = router.query
 
     return (
         <div>
             Sup {id}
+            <img src={car.image}></img>
         </div>
     );
 }
 
 export default Car;
+
+export const getStaticProps = async ({ params }) => {
+
+    const req = await fetch(`http://localhost:300/${params.id}.json`)
+    const data = await req.json()
+
+    return {
+        props: {
+            car: data
+        }
+    }
+}
