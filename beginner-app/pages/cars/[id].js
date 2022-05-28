3c65edd7c382b3f09f414b96d760c040970accbc
BossDaily <gboys2008@gmail.com>
2022-05-28 18:15:41 -0500

    static props are mid

--- beginner-app/pages/cars/[id].js
+++ beginner-app/pages/cars/[id].js
@@ -2,7 +2,6 @@ import { useRouter } from "next/router";
 import Head from "next/head";
 import styles from "../../styles/Home.module.css";
 
-
 const Car = ({ car }) => {
   const router = useRouter();
   const { id } = router.query;
@@ -22,27 +21,3 @@ const Car = ({ car }) => {
 
 export default Car;
 
- export async function getStaticProps({ params }) {
-
-     const req = await fetch(`http:localhost:3000/${params.id}.json`);
-     const data = await req.json();
-
-     return {
-         props: { car: data },
-     }
- }
-
- export async function getStaticPaths() {
-
-     const req = await fetch('http:localhost:3000/cars.json');
-     const data = await req.json();
-
-     const paths = data.map(car => {
-         return { params: { id: car } }
-     })
-
-     return {
-         paths,
-         fallback: false
-     };
- }
