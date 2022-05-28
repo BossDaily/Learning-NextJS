ef672a55596990c857ebe0c148225a05a088b328
BossDaily <gboys2008@gmail.com>
2022-05-28 17:57:03 -0500

    cars.json

--- beginner-app/pages/cars/[id].js
+++ beginner-app/pages/cars/[id].js
@@ -1,44 +1,48 @@
-import {useRouter} from 'next/router';
-import Head from 'next/head'
+import { useRouter } from "next/router";
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+
 
 const Car = ({ car }) => {
-    const router = useRouter()
-    const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
 
-    return (
-        <div>
-            Sup {id}
-            <img src={car.image}></img>
-        </div>
-    );
-}
+  return (
+    <>
+      <Head>
+        <title>
+          {car.color} {car.id}
+        </title>
+      </Head>
+      <h1>Sup {id}</h1>
+      <img src={car.image}></img>
+    </>
+  );
+};
 
 export default Car;
 
-export const getStaticProps = async ({ params }) => {
+ export async function getStaticProps({ params }) {
 
-    const req = await fetch(`http://localhost:3000/${params.id}.json`)
-    const data = await req.json()
+     const req = await fetch(`http:localhost:3000/${params.id}.json`);
+     const data = await req.json();
 
-    return {
-        props: {
-            car: data
-        }
-    }
-}
+     return {
+         props: { car: data },
+     }
+ }
 
-export const getStaticPaths = async () => {
-    const req = await fetch('http://localhost:3000/cars.json')
-    const data = await req.json()
+ export async function getStaticPaths() {
 
-    const paths = data.map(car => {
-        return {
-            params: { id: car}
-        }
-    })
+     const req = await fetch('http:localhost:3000/cars.json');
+     const data = await req.json();
 
-    return {
-        paths, 
-        fallback: false
-    }
-}
+     const paths = data.map(car => {
+         return { params: { id: car } }
+     })
+
+     return {
+         paths,
+         fallback: false
+     };
+ }
