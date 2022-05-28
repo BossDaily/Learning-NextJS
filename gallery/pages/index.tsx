c3b96037925a79a6fe8e7607f789b4d892c3eb98
BossDaily <gboys2008@gmail.com>
2022-05-28 15:08:20 -0500

    tried fixing the crappy formatting

--- gallery/pages/index.tsx
+++ gallery/pages/index.tsx
@@ -1,13 +1,29 @@
-import Images from "./_image";
-import Image from "next/image";
-import Gallery from "./_gallery";
-import { NextPage } from "next";
+import Image from "next/image"
+export default function Gallery() {
+    return (
+      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <BlurImage />
+        </div>
+      </div>
+    )
+  }
 
-const Home:NextPage = () => {
-  return(
-    <Gallery />
-  )
-  
-}
+  function BlurImage() {
+    return (
+      <a href='#' className='group'>
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <Image 
+            alt="" 
+            src="https://bit.ly/placeholder-img"
+            layout="fill"
+            objectFit="cover"
+            className="group-hover:opacity-75"
+          />
+          <h3 className="mt-4 text-sm text-gray-700">BossDaily</h3>
+          <p className="mt-1 text-lg font-medium text-gray-900">The goat</p>
+        </div>
+      </a>
+    )
+  }
 
-export default Home
