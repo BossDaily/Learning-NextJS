b57a1b0e51613aea11c5c4d2742363c95d84334d
BossDaily <gboys2008@gmail.com>
2022-05-29 17:28:29 -0500

    created a client

--- gallery/lib/BlurImage.tsx
+++ gallery/lib/BlurImage.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
+const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+)
+
 const cn = (...classes: string[]) => {
     return classes.filter(Boolean).join(' ')
 }
