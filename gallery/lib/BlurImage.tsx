d0a7019b5b0f6bc138836409ab0a3163c1a69501
BossDaily <gboys2008@gmail.com>
2022-05-29 17:10:51 -0500

    conditional logic

--- gallery/lib/BlurImage.tsx
+++ gallery/lib/BlurImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { useState } from "react";
 const BlurImage = () => {
     const [isLoading, setLoading] = useState(true)
-    
+
     return (
       <a href="#" className="group">
         <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
@@ -11,7 +11,13 @@ const BlurImage = () => {
             src="https://bit.ly/placeholder-img"
             layout="fill"
             objectFit="cover"
-            className="group-hover:opacity-75"
+            className={cn(
+                'group-hover:placeholder-opacity-75 duration-700 ease-in-out',
+                isLoading 
+                ? 'grayscale blur 2xl scale-110'
+                : 'grayscale-0 blur-0 0 scale-100'
+            )
+            }
           />
         </div>
         <h3 className="mt-4 text-sm text-gray-700">BossDaily</h3>
