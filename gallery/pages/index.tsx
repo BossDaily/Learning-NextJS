308f4e794b362126c8a24efb4a79d56572e9d28a
BossDaily <gboys2008@gmail.com>
2022-05-29 16:53:22 -0500

    imported image blur and removed function

--- gallery/pages/index.tsx
+++ gallery/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import BlurImage from "../lib/BlurImage";
 export default function Gallery() {
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -10,20 +11,3 @@ export default function Gallery() {
 }
 
 
-function BlurImage() {
-  return (
-    <a href="#" className="group">
-      <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-        <Image
-          alt=""
-          src="https://bit.ly/placeholder-img"
-          layout="fill"
-          objectFit="cover"
-          className="group-hover:opacity-75"
-        />
-      </div>
-      <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-      <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
-    </a>
-  );
-}
