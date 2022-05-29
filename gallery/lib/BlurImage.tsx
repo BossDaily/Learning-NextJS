4d04c2af1dcdce620959dea21ac3aedaae616b4a
BossDaily <gboys2008@gmail.com>
2022-05-29 16:54:25 -0500

    w

--- gallery/lib/BlurImage.tsx
+++ gallery/lib/BlurImage.tsx
@@ -5,14 +5,14 @@ const BlurImage = () => {
         <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
           <Image
             alt=""
-            src="https://bit.ly/placeholder-img"
+            src="https://avatars.githubusercontent.com/u/75937850?v=4"
             layout="fill"
             objectFit="cover"
             className="group-hover:opacity-75"
           />
         </div>
-        <h3 className="mt-4 text-sm text-gray-700">Lee Robinson</h3>
-        <p className="mt-1 text-lg font-medium text-gray-900">@leeerob</p>
+        <h3 className="mt-4 text-sm text-gray-700">BossDaily</h3>
+        <p className="mt-1 text-lg font-medium text-gray-900">The goat</p>
       </a>
     );
   }
