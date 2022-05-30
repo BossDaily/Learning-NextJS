4aaa17605c9f29b970ffc620a9488d27fad90eeb
BossDaily <gboys2008@gmail.com>
2022-05-30 12:29:22 -0500

    imported image properly and use it as a prop

--- gallery/pages/index.tsx
+++ gallery/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import {GetStaticProps} from 'next';
 import BlurImage from "../lib/BlurImage";
 import { createClient } from "@supabase/supabase-js";
+import Images from "../types/image"
 
 const cn = (...classes: string[]) => {
   return classes.filter(Boolean).join(' ')
@@ -26,7 +27,7 @@ export const getStaticProps:GetStaticProps = async () => {
 }
 
 
-const Home = () => {
+const Home = ({ images }: {images: Images[]}) => {
   return (
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
